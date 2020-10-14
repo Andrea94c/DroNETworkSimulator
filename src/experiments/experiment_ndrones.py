c5333e259fcfd7b4b476bfd5884c0b728c465e78
Andrea94c <andreacoletta94@live.it>
2020-10-14 22:27:22 +0200

    First Commit

---
+++ src/experiments/experiment_ndrones.py
@@ -0,0 +1,118 @@
+from src.utilities import config
+from src.simulation.simulator import Simulator
+import os
+from argparse import ArgumentParser
+
+LEN_TEST = 48000 # around 3hr of mission
+DELTA = 20
+
+def sim_setup(n_drones, seed, algorithm_routing):
+    """ return the sim setup """
+    len_simulation=LEN_TEST
+    time_step_duration=0.15 #150ms each step -> 72000 * 0.15 -> 3600*3 -> 3hr
+    env_width=1500
+    env_height=1500
+    drone_com_range=200
+    drone_sen_range=0
+    drone_speed=8 #18km/h
+    drone_max_buffer_size=500000
+    drone_max_energy=100000
+    drone_retransmission_delta=10
+    drone_communication_success=0.9 # TODO
+    depot_com_range=200
+    depot_coordinates=(750, 0)
+    event_duration=2000 #circa 5min
+    event_generation_prob=0.8
+    event_generation_delay=65
+    packets_max_ttl=len_simulation
+    show_plot=False
+    routing_algorithm = config.RoutingAlgorithm[algorithm_routing]
+    communication_error_type = config.ChannelError.GAUSSIAN
+
+    model_name = "model_" + str(n_drones)
+
+    return Simulator(
+                len_simulation=len_simulation,
+                 time_step_duration=time_step_duration,
+                 seed=seed,
+                 n_drones=n_drones,
+                 env_width=env_width,
+                 env_height=env_height,
+
+                 drone_com_range=drone_com_range,
+                 drone_sen_range=drone_sen_range,
+                 drone_speed=drone_speed,
+                 drone_max_buffer_size=drone_max_buffer_size,
+                 drone_max_energy=drone_max_energy,
+                 drone_retransmission_delta=drone_retransmission_delta,
+                 drone_communication_success=drone_communication_success,
+                 event_generation_delay = event_generation_delay,
+
+                 depot_com_range=depot_com_range,
+                 depot_coordinates=depot_coordinates,
+
+                 event_duration=event_duration,
+                 event_generation_prob=event_generation_prob,
+                 packets_max_ttl=packets_max_ttl,
+
+                 show_plot=show_plot,
+                 routing_algorithm=routing_algorithm,
+                 communication_error_type=communication_error_type,
+
+                 # ML parameters
+                 simulation_name=model_name,
+
+    )
+
+def exp_ndrones(path_filename, n_drones, in_seed, out_seed, algorithm_routing):
+    # ---- Experiment 1 ---- #
+    # test the routing for 
+    # at varying k and seed values
+    scores = {}
+    for seed in range(in_seed, out_seed):
+
+        print("Running " + algorithm_routing + " with", n_drones, "drones with seed:", seed)
+
+        simulation = sim_setup(n_drones, seed, algorithm_routing)
+        simulation.run()
+
+        simulation.save_metrics(path_filename + "out__ndrones_" + str(n_drones) +
+                            "_seed" + str(seed) + "_alg_" + algorithm_routing)
+        print("Score: ",simulation.score(), algorithm_routing)
+        scores[seed] = simulation.score()
+        simulation.close()
+
+    print("Ndrones: ", n_drones, " - Algo: ", algorithm_routing, "- Scores: ", scores)
+
+if __name__ == "__main__":
+    # parser input
+    parser = ArgumentParser()
+    
+    routing_choices = config.RoutingAlgorithm.keylist()
+
+    # MANDATORY
+    parser.add_argument("-nd", dest='number_of_drones', action="store", type=int,
+                        help="the number of drones to use in the simulataion")
+    parser.add_argument("-i_s", dest='initial_seed', action="store", type=int,
+                        help="the initial seed (included) to use in the simualtions")
+    parser.add_argument("-e_s", dest='end_seed', action="store", type=int,
+                        help="the end seed (excluded) to use in the simualtions"
+                             + "-notice that the simulations will run for seed in (i_s, e_s)")
+    parser.add_argument("-alg", dest='algorithm_routing', action="store", type=str,
+                        choices=routing_choices, help="the routing algorithm to use")
+
+
+    args = parser.parse_args()
+
+    number_of_drones = args.number_of_drones
+    initial_seed = args.initial_seed
+    end_seed = args.end_seed
+    algorithm_routing = args.algorithm_routing
+    path_filename = config.EXPERIMENTS_DIR
+
+    # build directories for results and models
+    os.system("mkdir " + path_filename)
+
+    exp_ndrones(path_filename, number_of_drones, initial_seed, end_seed, algorithm_routing)
+    print("Sim completed")    
+
---
+++ src/utilities/config.py
@@ -0,0 +1,123 @@
+
+from src.routing_algorithms.georouting import GeoRouting
+from src.routing_algorithms.random_routing import RandomRouting
+
+from enum import Enum
+
+
+"""
+This file contains all the constants and parameters of the simulator.
+It comes handy when you want to make one shot simulations, making parameters and constants vary in every
+simulation. For an extensive experimental campaign read the header at src.simulator.
+
+Attributes that one needs tweak often are tagged with # ***
+"""
+
+# ----------------------------------------------------------------------------------
+#
+#                  ██████  ██████  ███    ██ ███████ ██  ██████ 
+#                 ██      ██    ██ ████   ██ ██      ██ ██      
+#                 ██      ██    ██ ██ ██  ██ █████   ██ ██   ███ 
+#                 ██      ██    ██ ██  ██ ██ ██      ██ ██    ██ 
+#                  ██████  ██████  ██   ████ ██      ██  ██████  
+#
+# ----------------------------------------------------------------------------------
+
+
+
+# ----------------------- PATH DRONES -----------------------------------------#
+
+DEMO_PATH = False    # bool: whether to use handcrafted tours or not
+# to set up handcrafted torus see utilities.utilities
+PATH_FROM_JSON = False                   # bool: whether to use the path (for drones) store in the JSONS_PATH_PREFIX,
+                                            # otherwise path are generated online
+JSONS_PATH_PREFIX = "data/tours/RANDOM_missions{}.json"     # str: the path to the drones tours,
+                                            # the {} should be used to specify the seed -> es. data/tours/RANDOM_missions1.json for seed 1.
+RANDOM_STEPS = [250, 500, 700, 900, 1100, 1400]  # the step after each new random directions is taken, in case of dynamic generation
+RANDOM_START_POINT = True  # bool whether the drones start the mission at random positions
+
+# ------------------------------- CONSTANTS ------------------------------- #
+
+DEBUG = False                         # bool: whether to print debug strings or not.
+EXPERIMENTS_DIR = "data/experiments/"  # output data : the results of the simulation
+
+# drawaing
+PLOT_SIM = True      # bool: whether to plot or not the simulation.
+WAIT_SIM_STEP = 0     # float: seconds, pauses the rendering for 'DELAY_PLOT' seconds.
+SKIP_SIM_STEP = 5     # int: steps, plot the simulation every 'RENDERING_STEP' steps. At least 1.
+DRAW_SIZE = 700       # int: size of the drawing window.
+IS_SHOW_NEXT_TARGET_VEC = True  # bool : whether show the direction and next target of the drone
+
+SAVE_PLOT = False  # bool: whether to save the plots of the simulation or not.
+SAVE_PLOT_DIR = "data/plots/"
+
+
+# add constants here...
+
+# ----------------------------- SIMULATION PARAMS. ---------------------------- #
+SIM_DURATION = 70000   # int: steps of simulation. # ***
+TS_DURATION = 0.150   # float: seconds duration of a step in seconds.
+SEED = 20          # int: seed of this simulation.
+
+N_DRONES = 5      # int: number of drones. # ***
+ENV_WIDTH = 1500      # float: meters, width of environment.
+ENV_HEIGHT = 1500     # float: meters, height of environment.
+
+# events
+EVENTS_DURATION = 2000  # SIM_DURATION  # int: steps, number of time steps that an event lasts  -> to seconds = step * step_duration.
+D_FEEL_EVENT = 65      # int: steps, a new packet is felt (generated on the drone) every 'D_FEEL_EVENT' steps. # ***
+P_FEEL_EVENT = .8       # float: probability that the drones feels the event generated on the drone. # ***
+
+""" e.g. given D_FEEL_EVENT = 500, P_FEEL_EVENT = .5, every 500 steps with probability .5 the drone will feel an event."""
+
+# drones
+COMMUNICATION_RANGE_DRONE = 200  # float: meters, communication range of the drones.
+SENSING_RANGE_DRONE = 0        # float: meters, the sensing range of the drones.
+DRONE_SPEED = 8                  # float: m/s, drone speed.
+DRONE_MAX_BUFFER_SIZE = 100     # int: max number of packets in the buffer of a drone.
+DRONE_MAX_ENERGY = 1000000           # int: max energy of a drone.
+
+# depot
+DEPOT_COMMUNICATION_RANGE = 200  # float: meters, communication range of the depot.
+DEPOT_COO = (750, 0)             # (float, float): coordinates of the depot.
+
+
+# ------------------------------- ROUTING PARAMS. ------------------------------- #
+class RoutingAlgorithm(Enum):
+    GEO = GeoRouting
+    RND = RandomRouting
+
+    @staticmethod
+    def keylist():
+        return list(map(lambda c: c.name, RoutingAlgorithm))
+
+class ChannelError(Enum):
+    UNIFORM = 1
+    GAUSSIAN = 2
+    NO_ERROR = 3
+    
+    @staticmethod
+    def keylist():
+        return list(map(lambda c: c.name, ChannelError))
+
+
+ROUTING_ALGORITHM = RoutingAlgorithm.GEO
+CHANNEL_ERROR_TYPE = ChannelError.GAUSSIAN
+
+COMMUNICATION_P_SUCCESS = 1   # float: probability to have success in a communication.
+GUASSIAN_SCALE = .9            # float [0,1]: scale the error probability of the guassian -> success * GUASSIAN_SCALER
+PACKETS_MAX_TTL = 200         # float: threshold in the maximum number of hops. Causes loss of packets.
+RETRANSMISSION_DELAY = 10     # int: how many time steps to wait before transmit again (for k retransmissions). # ---  #delta_k
+
+# ------------------------------------------- ROUTING MISC --------------------------------- #
+HELLO_DELAY = 5            # int : how many time steps wait before transmit again an hello message
+RECEPTION_GRANTED = 0.95   # float : the min amount of success to evalute a neigh as relay
+LIL_DELTA = 1              # INT:  > 0
+
+ROOT_EVALUATION_DATA = "data/evaluation_tests/"
+
+NN_MODEL_PATH = "data/nnmodels/"
+
+# --------------- new cell probabilities -------------- #
+CELL_PROB_SIZE_R = 1.875  # the percentage of cell size with respect to drone com range
+

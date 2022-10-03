3ebe070c66f1084fb3712b7f139cba359a6b8924
flavio giorgi <giorgi.flavio.at@gmail.com>
2022-10-03 16:58:58 +0200

    Minor Refactoring

--- src/entities/uavs/drone.py
+++ src/entities/uavs/drone.py
@@ -1,48 +1,8 @@
 import numpy as np
 from src.entities.events.event import Event
 from src.entities.generic.entity import Entity
+from src.entities.uavs.depot import Depot
 from src.utilities import config, utilities
-
-
-class Depot(Entity):
-    """
-    The depot is an Entity
-    """
-
-    def __init__(self, simulator, coordinates, communication_range):
-        super().__init__(simulator=simulator, identifier=id(self), coordinates=coordinates)
-
-        self.communication_range = communication_range
-        self.__buffer = list()  # also with duplicated packets
-
-    @property
-    def all_packets(self):
-        """
-        Return all packets within the depot buffer
-        @return: a list of Packets
-        """
-
-        return self.__buffer
-
-    def transfer_notified_packets(self, drone):
-        """
-        This function is called when a Drone wants to offload packets to the depot
-        @param drone:
-        @return:
-        """
-
-        packets_to_offload = drone.all_packets
-        self.__buffer += packets_to_offload
-
-        for packet in packets_to_offload:
-
-            packet.time_delivery = self.simulator.cur_step
-
-            # add metrics: all the packets notified to the depot
-            self.simulator.logger.add_drones_packet_to_depot(timestep=self.simulator.cur_step,
-                                                             packet=packet,
-                                                             source_drone=drone)
-
 
 
 class Drone(Entity):
--- src/utilities/config.py
+++ src/utilities/config.py
@@ -45,7 +45,7 @@ DEBUG = False                           # bool: whether to print debug strings o
 EXPERIMENTS_DIR = "data/experiments/"  # output data : the results of the simulation
 
 # drawing
-PLOT_SIM = False      # bool: whether to plot or not the simulation.
+PLOT_SIM = True      # bool: whether to plot or not the simulation.
 WAIT_SIM_STEP = 0    # float: seconds, pauses the rendering for 'DELAY_PLOT' seconds.
 SKIP_SIM_STEP = 5    # int: steps, plot the simulation every 'RENDERING_STEP' steps. At least 1.
 DRAW_SIZE = 700      # int: size of the drawing window.
